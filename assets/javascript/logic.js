b603e8139a2ec1e858af3a9b4fb8473f0a73f39b
TGHeadle1371 <tgheadle@gmail.com>
2019-01-16 18:14:33 -0700

    More materialize fixes

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -21,13 +21,13 @@ $(document).ready(function () {
                 dataType: "json",
                 success: function (result, status, xhr) {
                     // Create a table with table rows holding table data related to weather
-                    var table = $("<table><tr><th>Weather Description</th></tr>").attr("id", "weatherInfo");
-                    table.append("<tr><td>City:</td><td>" + result.name + "</td></tr>");
+                    var table = $("<table><thead><tr>Weather Description</tr></thead>").attr("class", "z-depth-2 responsive-table centered");
+                    table.append("<tbody><tr><td>City:</td><td>" + result.name + "</td></tr>");
                     table.append("<tr><td>Country:</td><td>" + result.sys.country + "</td></tr>");
                     table.append("<tr><td>Current Temperature:</td><td>" + result.main.temp + "°F</td></tr>");
                     table.append("<tr><td>Humidity:</td><td>" + result.main.humidity + "</td></tr>");
                     table.append("<tr><td>Weather:</td><td>" + result.weather.description + "</td></tr>");
-                    table.append("<tr><td>Wind Speed:</td><td>" + result.wind.speed + "</td></tr>");
+                    table.append("<tr><td>Wind Speed:</td><td>" + result.wind.speed + "</td></tr></tbody></table>");
                     $("#weatherInfo").html(table);
                 },
                 // If theres an error, log the error
