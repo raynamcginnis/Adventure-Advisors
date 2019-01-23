4fe51faa5362a645aee42ef5d559bf56f0c8ddec
TGHeadle1371 <tgheadle@gmail.com>
2019-01-22 19:19:34 -0700

    style edits for firebase table

--- assets/javascript/firebase.js
+++ assets/javascript/firebase.js
@@ -1,39 +1,38 @@
-
 // Initialize Firebase
 var config = {
-  apiKey: "AIzaSyBNxXvHwFMZknaZ4GNpH7AJl4QZmS15-WM",
-  authDomain: "adventureadvisors-e98a1.firebaseapp.com",
-  databaseURL: "https://adventureadvisors-e98a1.firebaseio.com",
-  projectId: "adventureadvisors-e98a1",
-  storageBucket: "adventureadvisors-e98a1.appspot.com",
-  messagingSenderId: "568711330769"
+    apiKey: "AIzaSyBNxXvHwFMZknaZ4GNpH7AJl4QZmS15-WM",
+    authDomain: "adventureadvisors-e98a1.firebaseapp.com",
+    databaseURL: "https://adventureadvisors-e98a1.firebaseio.com",
+    projectId: "adventureadvisors-e98a1",
+    storageBucket: "adventureadvisors-e98a1.appspot.com",
+    messagingSenderId: "568711330769"
 };
 firebase.initializeApp(config);
-
+// Firebase Database
 var database = firebase.database();
 
-
+// On Favorite button click
 $("#favBtn").on("click", function (event) {
-  event.preventDefault();
+    event.preventDefault();
+    // Favorites  = SearchText Input
+    var favorite = $("#searchText").val().trim();
+    // establish FireBase Favorites
+    var favPlace = {
+        favorite: favorite
+    };
 
-  var favorite = $("#searchText").val().trim();
-
-  var favPlace = {
-    favorite: favorite
-  }
-
-
-  database.ref().push(favPlace)
-  console.log(favPlace);
+    // Establish database reference, push favorite places to database
+    database.ref().push(favPlace);
+    //   console.log(favPlace);
 });
-
+// Add snapshot value to new favorite places to append into new row
 database.ref().on("child_added", function (childSnapshot) {
 
-  var newFavPlace = childSnapshot.val().favorite;
-  console.log(newFavPlace);
+    var newFavPlace = childSnapshot.val().favorite;
+    //   console.log(newFavPlace);
 
-  var newRow = $("<tr>").append(
-    $("<td>").text(newFavPlace)
-  );
-  $("#favorite").append(newRow);
+    var newRow = $("<tr>").append(
+        $("<td>").text(newFavPlace.toUpperCase())
+    );
+    $("#favorite").prepend(newRow);
 });
